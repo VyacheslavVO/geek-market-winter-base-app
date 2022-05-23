1f6e0b770c99ab252c76cf4b7716970cd85b9336
VyacheslavVO <vyacheslavvo@mail.ru>
2022-05-23 11:45:10 +0300

    Домашнее задание, урок 02

--- src/main/java/com/geekbrains/geekmarketwinter/GeekMarketWinterApplication.java
+++ src/main/java/com/geekbrains/geekmarketwinter/GeekMarketWinterApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
-public class GeekMarketWinterApplication {
+class GeekMarketWinterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeekMarketWinterApplication.class, args);
 	}
